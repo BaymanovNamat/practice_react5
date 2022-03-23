5bd6065caa27001e53f6f18f3f9d74a516bf255c
Namat Bayamanov <namataxvd15@gmail.com>
2022-03-23 14:50:50 +0600

    Added paragraph component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
+import Paragraph from "./components/Paragraph/Paragraph";
 
 function App() {
+
+
+  const app = (
+    <>
+      <h1>app</h1>
+      <p>sdjchwkcgwlcgwkjcgwkcgwk</p>
+    </>
+  );
+
+
+
   return (
     <div className="App">
-      
+      <h2>
+        Hello world
+      </h2>
+      <Paragraph size="24px">
+        This is paragraph
+      </Paragraph>
+
+
+      <Paragraph size=".5rem">
+        This is small paragraph
+      </Paragraph>
     </div>
   );
 }
---
+++ src/components/Paragraph/Paragraph.js
@@ -0,0 +1,17 @@
+function Paragraph(props) {
+
+  const style ={
+    fontSize: props.size
+  }
+
+
+
+
+  return (
+    <p style={style}>
+      {props.children}
+    </p>
+  );
+}
+
+export default Paragraph;
